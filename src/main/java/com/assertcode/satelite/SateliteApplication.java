7c8916d3979952ed7ecf92c6ce8e874e46f72515
Tiberio Lima <tiberio.lima@gmail.com>
2023-06-02 21:06:41 -0300

    Project created

---
+++ src/main/java/com/assertcode/satelite/SateliteApplication.java
@@ -0,0 +1,13 @@
+package com.assertcode.satelite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SateliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SateliteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assertcode/satelite/SateliteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assertcode.satelite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SateliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
